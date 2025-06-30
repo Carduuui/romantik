0b1e9246566f25fa226e3f74deaf8d5c3c05cc72
Carduuui <eckstein.jakob1@gmail.com>
2025-06-30 21:02:41 +0200

    test

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head(){
+    return(
+        <>
+      <title>Romantik</title>
+      <link rel="icon"href="/logo_files/logo.png"/>
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,8 +77,7 @@ export default function RomanticismWebsite() {
 
   return (
     <>
-      <title>Romantik</title>
-      <link rel="icon"href="/logo_files/logo.png"/>
+
     <div className="min-h-screen bg-gradient-to-br from-purple-50 via-rose-50 to-amber-50">
       {/* Header */}
       <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
